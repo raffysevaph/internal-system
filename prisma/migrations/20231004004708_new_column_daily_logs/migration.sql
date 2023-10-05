4dd9c24af6eb81e85558ecb16417d9140f9a4bb5
Rafael Seva <rafaelsevaiv@gmail.com>
2023-10-05 18:39:56 +0800

    updated schema and migrations

---
+++ prisma/migrations/20231004004708_new_column_daily_logs/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "daily_logs" ADD COLUMN     "deletedAt" TIMESTAMP(3);
---
+++ prisma/migrations/20231004011119_new_column_daily_logs_test_column/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "daily_logs" ADD COLUMN     "testColumn" INTEGER NOT NULL DEFAULT 0;
