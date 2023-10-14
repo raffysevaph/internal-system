2b7fc02a28e75c3554d0b69888aa0403be28ce9a
Rafael Seva <rafaelsevaiv@gmail.com>
2023-10-14 20:34:41 +0800

    added some test migrations

---
+++ prisma/migrations/20231006095530_added_new_column_testing/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "daily_logs" ADD COLUMN     "temporary" TEXT;
---
+++ prisma/migrations/20231006100651_remove_new_column_testing/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `temporary` on the `daily_logs` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "daily_logs" DROP COLUMN "temporary";
