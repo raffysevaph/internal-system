107805dfc67a58cf312687472007ed6c7a17b3db
Rafael Seva <rafaelsevaiv@gmail.com>
2023-10-21 14:13:22 +0800

    added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import axios from "axios";
 import { useRouter } from "next/navigation";
 
+import Sidebar from "@src/components/Sidebar";
+
 export default function Home() {
   const { push } = useRouter();
   const handleLogout = () => {
@@ -13,7 +15,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Sidebar />
       <div>
+        <h1>hello world</h1>
         <button onClick={handleLogout}>Logout</button>
       </div>
     </main>
